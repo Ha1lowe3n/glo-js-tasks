3250755b87627acf79cb6455f569b93701163c33
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-10-10 18:07:27 +0800

    fix(script.js): fix all

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const money = 50000,
-      income = "Frilance",
-      addExpenses = "internet, taxi, study",
-      deposit = true,
-      mission = 1000000,
-      period = 8;
+  income = "Frilance",
+  addExpenses = "internet, taxi, study",
+  deposit = true,
+  mission = 1000000,
+  period = 8;
 
 alert("Hello, it's my first homework");
 console.log("I'm here too");
@@ -18,7 +18,7 @@ console.log(`Цель заработать ${mission} рублей`);
 console.log(addExpenses.toLowerCase().split(', '));
 
 // добавил метод toFixed, т.к. "неудобно" смотреть на большое число в консоли
-let budgetDay = Math.round(money / 30);
+const budgetDay = Math.round(money / 30);
 console.log(budgetDay);
 
 
