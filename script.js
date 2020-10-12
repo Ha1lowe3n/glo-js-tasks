5d3bc3b36265326c0ae3bfef20c592712b22a08c
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-10-12 19:44:56 +0800

    feat(script.js): new feat

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const money = 50000,
-  income = "Frilance",
+'use strict';
+
+let money = 50000,
   addExpenses = "internet, taxi, study",
-  deposit = true,
+  deposit = true;
+
+const income = "Frilance",
   mission = 1000000,
   period = 8;
 
@@ -17,9 +20,40 @@ console.log(`Цель заработать ${mission} рублей`);
 
 console.log(addExpenses.toLowerCase().split(', '));
 
-// добавил метод toFixed, т.к. "неудобно" смотреть на большое число в консоли
-const budgetDay = Math.round(money / 30);
+let budgetDay = Math.round(money / 30);
 console.log(budgetDay);
+
+
+// lesson 3 
+money = prompt('Ваш месячный доход?');
+
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартплата, проездной, кредит');
+
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+const expensesOne = prompt('Введите обязательную статью расходов?'),
+  amountOne = prompt('Во сколько это обойдется?'),
+  expensesTwo = prompt('Введите обязательную статью расходов?'),
+  amountTwo = prompt('Во сколько это обойдется?');
+
+
+const budgetMonth = +money - +amountOne - +amountTwo;
+console.log(budgetMonth);
+
+const missionMonth = Math.ceil(mission / budgetMonth);
+
+budgetDay = budgetMonth / 30;
+console.log(Math.floor(budgetDay));
+
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (1200 > budgetDay >= 600) {
+  console.log('У вас средний уровень дохода');
+} else if (600 > budgetDay >= 0) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+  console.log('Что то пошло не так');
+}
 
 
 
