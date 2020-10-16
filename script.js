ddf1bb47af8c4011161ffd3ea5e313f13eb394ad
Igor Trifonov <Ha1lowe3n@yandex.ru>
2020-10-16 23:12:03 +0800

    fix(script.js): add const

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // генерация числа
 const randomNumber = function (min, max) {
     // получить случайное число от (min-0.5) до (max+0.5)
-    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    const rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
 };
 
